877a53447d55a4ab7097e396749127b33d702ad8
TSoli <tariqsoliman2000@gmail.com>
2023-12-01 19:27:41 +1000

    completed day 1

---
+++ 2023/day01b/solution.cpp
@@ -0,0 +1,77 @@
+#include <string>
+#include <string_view>
+#include <fstream>
+#include <iostream>
+#include <ranges>
+#include <unordered_map>
+#include <iterator>
+#include <limits.h>
+
+template <typename Iter>
+int getDigit(Iter begin, Iter end, bool fromEnd = false);
+
+const std::unordered_map<std::string, int> DIGITS = {
+  {"zero", 0},
+  {"one", 1},
+  {"two", 2},
+  {"three", 3},
+  {"four", 4},
+  {"five", 5},
+  {"six", 6},
+  {"seven", 7},
+  {"eight", 8},
+  {"nine", 9}
+};
+
+int main() {
+  std::string filename = "input.txt";
+  std::ifstream inf(filename);
+  if (!inf) {
+    std::cerr << "Couldn't open " + filename +"\n";
+    return 1;
+  }
+
+  int sum = 0;
+  while (inf) {
+    std::string line;
+    inf >> line;
+    sum += 10 * getDigit(line.begin(), line.end());
+    sum += getDigit(line.rbegin(), line.rend(), true);
+  }
+
+  std::cout << sum << std::endl;
+  return 0;
+}
+
+template <typename Iter>
+int getDigit(Iter begin, Iter end, bool fromEnd) {
+  int pos = INT_MAX;
+  int digit = 0;
+  std::string line(begin, end);
+
+  for (auto it = begin; it != end; ++it) {
+    if (std::isdigit(*it)) {
+      pos = it - begin;
+      digit = *it - '0';
+      break;
+    }
+  }
+
+  for (auto& [str, num] : DIGITS) {
+    Iter it;
+    int currPos = -1;
+    if (fromEnd) {
+      std::string revStr(str.rbegin(), str.rend());
+      currPos = line.find(revStr);
+    } else {
+      currPos = line.find(str);
+    }
+
+    if (currPos != std::string::npos && currPos < pos) {
+      pos = currPos;
+      digit = num;
+    }
+  }
+
+  return digit;
+}
