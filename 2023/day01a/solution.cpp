ae2c652c5bfcd82ddea71667f8d4a659ee71e92f
TSoli <tariqsoliman2000@gmail.com>
2023-12-01 19:27:41 +1000

    completed day 1

---
+++ 2023/day01a/solution.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <ranges>
+
+int main() {
+  std::string filename = "input.txt";
+  std::ifstream inf(filename);
+  if (!inf) {
+    std::cerr << "Couldn't open " + filename +"\n";
+    return 1;
+  }
+
+  int sum = 0;
+  while (inf) {
+    std::string line;
+    inf >> line;
+    for (auto ch : line) {
+      if (std::isdigit(ch)) {
+        sum += 10 * (ch - '0');
+        break;
+      }
+    }
+
+    for (auto ch : std::ranges::views::reverse(line)) {
+      if (std::isdigit(ch)) {
+        sum += ch - '0';
+        break;
+      }
+    }
+  }
+
+  std::cout << sum << std::endl;
+  return 0;
+}
