f873a52bd5dc4524c3bc6a6c2e4fefe72ea2b8e5
TSoli <tariqsoliman2000@gmail.com>
2023-12-05 23:33:56 +1000

    refactored day 5 task 2 a bit

--- 2023/day05b/solution.cpp
+++ 2023/day05b/solution.cpp
@@ -5,7 +5,6 @@
 #include <map>
 #include <sstream>
 #include <string>
-#include <utility>
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -54,38 +53,31 @@ int main(int argc, char *argv[]) {
       --it;
     }
 
-    // This is disgusting and I should try to refactor it lmao
+    // still not great...
     while ((*it).first < from + range && it != seeds.end()) {
       if ((*it).first + (*it).second < from) {
+        // end of current seed range is before the transformation range
         ++it;
         continue;
       }
-      if (from + range >= (*it).first + (*it).second) {
+
+      if ((*it).first < from) {
+        // seed start is before the transformation start
+        transformed[to] =
+            std::min((*it).first + (*it).second, from + range) - from;
+        (*it).second = from - (*it).first;
+        ++it;
+      } else if (from + range >= (*it).first + (*it).second) {
         // seeds end is before the transformation range end
-        if ((*it).first < from) {
-          // seed start is before the transformation start
-          transformed[to] = (*it).first + (*it).second - from;
-          (*it).second = from - (*it).first;
-          ++it;
-        } else {
-          // seed start is after the transformation start
-          // => seed range is fully contained in transform
-          transformed[to + ((*it).first - from)] = (*it).second;
-          it = seeds.erase(it);
-        }
+        // => seed range is fully contained in transform
+        transformed[to + ((*it).first - from)] = (*it).second;
+        it = seeds.erase(it);
       } else {
         // end of transform is before the seeds end
         seeds[from + range] = (*it).first + (*it).second - from - range;
-        if ((*it).first < from) {
-          // seeds start is before the transformation start
-          transformed[to] = (*it).first + (*it).second - from;
-          (*it).second = from - (*it).first;
-          ++it;
-        } else {
-          // seeds start is after transformation start
-          transformed[to + ((*it).first - from)] = from + range - (*it).first;
-          it = seeds.erase(it);
-        }
+        // seeds start is after transformation start
+        transformed[to + ((*it).first - from)] = from + range - (*it).first;
+        it = seeds.erase(it);
       }
     }
   }
@@ -95,10 +87,6 @@ int main(int argc, char *argv[]) {
   }
   seeds = transformed;
   transformed.clear();
-
-  for (const auto &[start, range] : seeds) {
-    std::cout << "Start: " << start << " Range: " << range << "\n";
-  }
 
   std::cout << "Min location: " << (*(seeds.begin())).first << "\n";
   return 0;
