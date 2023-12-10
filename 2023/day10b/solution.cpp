6e9586ab4ca1430bc1dab97f063b7ef39ce50d28
Miguel Angel De la Vega Rodríguez <93781730+Miguevrgo@users.noreply.github.com>
2023-12-10 17:37:45 +0100

    Improves running time using hash

--- 2023/day10b/solution.cpp
+++ 2023/day10b/solution.cpp
@@ -3,15 +3,25 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using Edge = std::pair<int, int>;
 using Graph = std::vector<std::string>;
 
+struct pair_hash {
+    std::size_t operator () (const std::pair<int, int> &p) const {
+        std::size_t h1 = std::hash<int>{}(p.first);
+        std::size_t h2 = std::hash<int>{}(p.second);
+
+        return h1 ^ h2;
+    }
+};
+
 bool updateStartPipe(Graph &graph, const std::pair<int, int> &startPos);
 std::vector<std::pair<int, int>> getPath(const Graph &graph,
                                          const std::pair<int, int> &startPos);
-int getAreaEnclosed(const Graph &graph, std::vector<std::pair<int, int>> path);
+int getAreaEnclosed(const std::vector<std::string> &graph, std::unordered_set<std::pair<int, int>, pair_hash> path);
 
 const Edge kUp{-1, 0};
 const Edge kRight{0, 1};
@@ -62,8 +72,9 @@ int main(int argc, char *argv[]) {
     return 3;
   }
 
-  std::vector<std::pair<int, int>> path{getPath(graph, startPos)};
-  std::cout << "Area enclosed is: " << getAreaEnclosed(graph, path) << "\n";
+    std::vector<std::pair<int, int>> pathVector = getPath(graph, startPos);
+    std::unordered_set<std::pair<int, int>, pair_hash> path(pathVector.begin(), pathVector.end());
+    std::cout << "Area enclosed is: " << getAreaEnclosed(graph, path) << "\n";
 
   return 0;
 }
@@ -128,45 +139,33 @@ std::vector<std::pair<int, int>> getPath(const Graph &graph,
   return path;
 }
 
-int getAreaEnclosed(const Graph &graph, std::vector<std::pair<int, int>> path) {
-  int area{0};
-  for (int i = 0; i < graph.size(); ++i) {
-    int parity{0};
-    char enterPipe{0};
-    for (int j = 0; j < graph[i].size(); ++j) {
-      auto pathPos{std::ranges::find(path, std::pair<int, int>(i, j))};
-      bool inPath{pathPos != path.end()};
+int getAreaEnclosed(const std::vector<std::string> &graph, std::unordered_set<std::pair<int, int>, pair_hash> path) {
+    int area = 0;
+    for (int i = 0; i < graph.size(); ++i) {
+        int parityBit = 0;
+        char enterPipe = 0;
+        for (int j = 0; j < graph[i].size(); ++j) {
+            Edge currentPos(i, j);
+            bool inPath = path.count(currentPos);
 
-      if (!inPath) {
-        if (parity % 2) {
-          // You are only inside the path if you have crossed it an odd number
-          // of times
-          ++area;
-        }
-        continue;
-      } else {
-        // in the path
-        path.erase(pathPos);
-        char pipe{graph[i][j]};
+            if (!inPath){
+                if (parityBit){
+                    ++area;
+                }
+                continue;
+            }
 
-        switch (pipe) {
-          case '|':
-            ++parity;
-            break;
-          // You must be entering the path on the below
-          case 'F':
-          case 'L':
-            enterPipe = pipe;
-            break;
-          // You must be exiting the path on the below
-          case '7':
-            parity += enterPipe == 'L';
-            break;
-          case 'J':
-            parity += enterPipe == 'F';
+            path.erase(currentPos);
+
+            char pipe = graph[i][j];
+
+            if (pipe == '|' || pipe == '7' && enterPipe == 'L' || pipe == 'J' && enterPipe == 'F'){
+                parityBit ^= 1;
+            }
+            else if (pipe == 'F' || pipe == 'L'){
+                enterPipe = pipe;
+            }
         }
-      }
     }
-  }
-  return area;
+    return area;
 }
