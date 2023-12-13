0e0f7459d52f9148910a58e43b127e35efe939f3
TSoli <tariqsoliman2000@gmail.com>
2023-12-13 10:31:14 +1000

    completed day 12

---
+++ 2023/day12a/solution.cpp
@@ -0,0 +1,216 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<int> getNums(std::string_view numList);
+std::vector<std::pair<char, int>> getContiguousBlocks(std::string_view springs);
+bool trimSprings(std::vector<int> &nums,
+                 std::vector<std::pair<char, int>> &contiguousBlocks);
+bool removeBlocks(std::vector<std::pair<char, int>> &blocks, int numToRemove);
+int getArrangmenets(std::vector<int> nums,
+                    std::vector<std::pair<char, int>> contiguousBlocks);
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <filename>\n";
+    return 1;
+  }
+
+  std::ifstream inf{argv[1]};
+
+  if (!inf) {
+    std::cerr << "Could not open file: " << argv[1] << "\n";
+    return 2;
+  }
+
+  std::string springs;
+  std::string numList;
+
+  int arrangements{0};
+  while (inf >> springs) {
+    inf >> numList;
+    std::vector<int> nums{getNums(numList)};
+    std::vector<std::pair<char, int>> contiguousBlocks{
+        getContiguousBlocks(springs)};
+
+    arrangements += getArrangmenets(nums, contiguousBlocks);
+  }
+
+  std::cout << "Num arrangements: " << arrangements << "\n";
+
+  return 0;
+}
+
+std::vector<int> getNums(std::string_view numList) {
+  std::vector<int> nums;
+  auto it{numList.begin()};
+
+  while (it <= numList.end()) {
+    auto nextIt{std::find(it, numList.end(), ',')};
+    nums.push_back(std::stoi(std::string(it, nextIt)));
+    it = nextIt + 1;
+  }
+
+  return nums;
+}
+
+std::vector<std::pair<char, int>> getContiguousBlocks(
+    std::string_view springs) {
+  std::vector<std::pair<char, int>> contiguousBlocks{{springs.front(), 0}};
+  for (char ch : springs) {
+    if (ch != contiguousBlocks.back().first) {
+      contiguousBlocks.push_back({ch, 1});
+    } else {
+      contiguousBlocks.back().second++;
+    }
+  }
+
+  return contiguousBlocks;
+}
+
+bool trimSprings(std::vector<int> &nums,
+                 std::vector<std::pair<char, int>> &contiguousBlocks) {
+  auto numIt{nums.begin()};
+  auto blockIt{contiguousBlocks.begin()};
+
+  while (numIt != nums.end() || blockIt != contiguousBlocks.end()) {
+    if (blockIt == contiguousBlocks.end()) {
+      // there are still numbers left though!
+      return false;
+    }
+
+    if (blockIt->first == '.') {
+      blockIt = contiguousBlocks.erase(blockIt);
+    } else if (blockIt->first == '#') {
+      if (numIt == nums.end()) {
+        // there are no more to remove
+        return false;
+      }
+
+      if (!removeBlocks(contiguousBlocks, *numIt)) {
+        return false;
+      }
+
+      numIt = nums.erase(numIt);
+      blockIt = contiguousBlocks.begin();
+    } else {
+      // the '?' character
+      if (numIt == nums.end()) {
+        blockIt = contiguousBlocks.erase(blockIt);
+        continue;
+      }
+
+      if (blockIt->second < *numIt && (blockIt + 1) != contiguousBlocks.end() &&
+          (blockIt + 1)->first == '.') {
+        blockIt = contiguousBlocks.erase(blockIt);
+      } else {
+        // can't trim anymore
+        return true;
+      }
+    }
+  }
+
+  return true;
+}
+
+bool removeBlocks(std::vector<std::pair<char, int>> &blocks, int numToRemove) {
+  auto blockIt{blocks.begin()};
+
+  while (numToRemove) {
+    if (blockIt == blocks.end()) {
+      return false;
+    }
+
+    if (blockIt->second == 0) {
+      blockIt = blocks.erase(blockIt);
+      continue;
+    }
+
+    if (blockIt->first == '.') {
+      return false;
+    }
+
+    if (blockIt->first == '#' && blockIt->second > numToRemove) {
+      return false;
+    }
+
+    if (blockIt->first == '#') {
+      numToRemove -= blockIt->second;
+      blockIt = blocks.erase(blockIt);
+      if (blockIt != blocks.end() && blockIt->first == '?' &&
+          numToRemove == 0) {
+        if (blockIt->second == 1) {
+          blockIt = blocks.erase(blockIt);
+        } else {
+          blockIt->second--;
+        }
+      }
+      continue;
+    }
+    // it's a '?'
+    blockIt->second -= numToRemove;
+    if (blockIt->second < 0) {
+      numToRemove = -blockIt->second;
+      blockIt = blocks.erase(blockIt);
+    } else if (blockIt->second == 0 && (blockIt + 1) != blocks.end()) {
+      if ((blockIt + 1)->first == '#') {
+        return false;
+      }
+      // next is '.'
+      blocks.erase(blockIt);
+      break;
+    } else if (blockIt->second == 1 || blockIt->second == 0) {
+      blocks.erase(blockIt);
+      break;
+    } else {
+      // you need a '.' to break the sequence of '#'
+      blockIt->second--;
+      break;
+    }
+  }
+
+  return true;
+}
+
+int getArrangmenets(std::vector<int> nums,
+                    std::vector<std::pair<char, int>> contiguousBlocks) {
+  bool isTrimmed{trimSprings(nums, contiguousBlocks)};
+
+  if (!isTrimmed) {
+    return 0;
+  }
+
+  if (nums.size() == 0 && contiguousBlocks.size() == 0) {
+    return 1;
+  }
+
+  // must have '?' at the start
+  // assume no fault at the start
+  std::vector<std::pair<char, int>> blocksNoFault{contiguousBlocks};
+  if (blocksNoFault[0].second == 1) {
+    blocksNoFault.erase(blocksNoFault.begin());
+  } else {
+    blocksNoFault[0].second--;
+  }
+
+  auto numsNoFault{nums};
+
+  // Try placing faulty spring at the start
+  contiguousBlocks[0].second--;
+  if (contiguousBlocks[0].second == 0) {
+    // change it to a '#'
+    contiguousBlocks.erase(contiguousBlocks.begin());
+  }
+
+  if (contiguousBlocks[0].first != '#') {
+    // add '#' at the start
+    contiguousBlocks.insert(contiguousBlocks.begin(), {'#', 1});
+  } else {
+    contiguousBlocks[0].second++;
+  }
+
+  return getArrangmenets(nums, contiguousBlocks) +
+         getArrangmenets(numsNoFault, blocksNoFault);
+}
