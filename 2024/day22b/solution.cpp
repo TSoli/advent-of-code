a4bfbbc5ec05cac02aeea13d1f259f893aa08233
TSoli <tariqsoliman2000@gmail.com>
2024-12-22 20:04:31 +1000

    Optimised day 22

--- 2024/day22b/solution.cpp
+++ 2024/day22b/solution.cpp
@@ -2,25 +2,13 @@
 #include <deque>
 #include <fstream>
 #include <iostream>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 const long kPrune{16777216};
 
 using Changes = std::array<int, 4>;
-using ChangeTracker = std::unordered_map<std::array<int, 4>, long>;
-using ChangeSet = std::unordered_set<std::array<int, 4>>;
-
-template <> struct std::hash<std::array<int, 4>> {
-  size_t operator()(const std::array<int, 4> &arr) const {
-    size_t seed = 0;
-    for (int i = 0; i < arr.size(); ++i) {
-      seed ^= (std::hash<int>()(arr[i]) << i);
-    }
-    return seed;
-  }
-};
+using ChangeTracker = std::array<int, ((1 << 20) - 1)>;
+using ChangeSet = std::array<bool, ((1 << 20) - 1)>;
 
 int advanceSecret(long n) {
   long m{n << 6};
@@ -28,7 +16,6 @@ int advanceSecret(long n) {
   n %= kPrune;
   m = n >> 5;
   n ^= m;
-  n %= kPrune;
   m = n << 11;
   n ^= m;
   return n % kPrune;
@@ -53,11 +40,11 @@ int main(int argc, char *argv[]) {
     secrets.push_back(n);
   }
 
-  ChangeTracker changeTracker;
+  ChangeTracker changeTracker{};
   for (int i = 0; i < secrets.size(); ++i) {
     std::vector<int> vals(2001, 0);
     std::deque<int> changes;
-    ChangeSet changeSet;
+    ChangeSet changeSet{};
     vals[0] = secrets[i];
 
     for (int j = 1; j < 2001; ++j) {
@@ -67,27 +54,26 @@ int main(int argc, char *argv[]) {
 
       changes.push_back(vals[j] - vals[j - 1]);
 
-      if (j < 4)
-        continue;
+      if (j < 4) continue;
 
-      if (j > 4)
-        changes.pop_front();
+      if (j > 4) changes.pop_front();
 
-      std::array<int, 4> currChanges;
-      for (size_t k = 0; k < currChanges.size(); ++k) {
-        currChanges[k] = changes[k];
+      int change{0};
+      for (size_t k = 0; k < changes.size(); ++k) {
+        // numbers in range [-9, 9] only require 5 bits so we can pack them all
+        // into 20 bits
+        change |= (changes[k] & ((1 << 5) - 1)) << (k * 5);
       }
 
-      if (changeSet.contains(currChanges))
-        continue;
+      if (changeSet[change]) continue;
 
-      changeSet.insert(currChanges);
-      changeTracker[currChanges] += vals[j];
+      changeSet[change] = true;
+      changeTracker[change] += vals[j];
     }
   }
 
-  long maxBananas{0};
-  for (const auto &[k, b] : changeTracker) {
+  int maxBananas{0};
+  for (const auto b : changeTracker) {
     maxBananas = std::max(maxBananas, b);
   }
 
