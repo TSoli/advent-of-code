bbad76f89a0f950185892a14485c7e6a9060142e
TSoli <tariqsoliman2000@gmail.com>
2025-12-10 22:35:40 +1000

    Made day02b efficient

--- 2025/day02b/solution.cpp
+++ 2025/day02b/solution.cpp
@@ -1,46 +1,73 @@
 #include <aoc/io.h>
 
-#include <algorithm>
 #include <cmath>
 #include <fstream>
 #include <iostream>
-#include <limits>
-#include <ranges>
+#include <set>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-bool isInvalid(const string &id) {
-  size_t len{id.length()};
-  size_t i{2};
-  while (len / i) {
-    if (len % i) {
-      ++i;
-      continue;
-    }
-
-    vector<string> substrings;
-    for (size_t j = 0; j + (len / i) <= len; j += len / i) {
-      substrings.push_back(id.substr(j, len / i));
-    }
-
-    if (ranges::all_of(substrings,
-                       [&](const auto &s) { return s == substrings.front(); }))
-      return true;
-
-    ++i;
-  }
-  return false;
-}
-
-long getInvalidIds(long start, long end) {
+long getInvalidIds(const string &start, const string &end) {
   long total{0};
-  while (start <= end) {
-    if (isInvalid(to_string(start))) {
-      total += start;
+  string s{start};
+
+  while (s.length() < end.length()) {
+    string e{to_string(static_cast<long>(pow(10, s.length())) - 1)};
+    total += getInvalidIds(s, e);
+    s = to_string(static_cast<long>(pow(10, s.length())));
+  }
+
+  int len{static_cast<int>(s.length())};
+  set<string> invalidIds;
+  for (int i = 2; len / i > 0; ++i) {
+    if (len % i)
+      continue;
+
+    vector<long> lower;
+    vector<long> upper;
+
+    for (size_t j = 0; j + (len / i) <= len; j += len / i) {
+      lower.push_back(stol(s.substr(j, len / i)));
+      upper.push_back(static_cast<long>(pow(10, len / i) - 1));
     }
-    ++start;
+
+    upper[0] = stol(end.substr(0, len / i));
+
+    while (lower[0] <= upper[0]) {
+      int k = 1;
+      while (k < lower.size() && lower[k - 1] == upper[k - 1]) {
+        upper[k] = stol(end.substr(k * (len / i), len / i));
+        ++k;
+      }
+
+      bool isInRange{true};
+      for (int k = 0; k < lower.size(); ++k) {
+        if (lower[0] > lower[k] && lower[0] < upper[k])
+          break;
+        if (lower[0] < lower[k] || lower[0] > upper[k]) {
+          isInRange = false;
+          break;
+        }
+      }
+
+      string id;
+      for (int n = 0; n < i; ++n) {
+        id += to_string(lower[0]);
+      }
+
+      if (!invalidIds.contains(id) && isInRange) {
+        total += stol(id);
+        invalidIds.insert(id);
+      }
+
+      for (int k = 1; k < lower.size(); ++k) {
+        lower[k] = 0;
+      }
+
+      ++lower[0];
+    }
   }
 
   return total;
@@ -57,9 +84,10 @@ int main(int argc, char *argv[]) {
     string start{idRange.substr(0, splitPos)};
     string end{idRange.substr(splitPos + 1)};
 
-    if (end.back() == '\n') end.pop_back();
+    if (end.back() == '\n')
+      end.pop_back();
 
-    long numInvalid{getInvalidIds(stol(start), stol(end))};
+    long numInvalid{getInvalidIds(start, end)};
     total += numInvalid;
   }
 
--- 2025/day03b/solution.cpp
+++ 2025/day03b/solution.cpp
@@ -2,7 +2,6 @@
 
 #include <fstream>
 #include <iostream>
-#include <ranges>
 #include <string>
 #include <vector>
 
